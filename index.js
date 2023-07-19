0bacdfb43ab9b552bb83bcaa95c96e7975c0a6ad
agwadan <agwa.dan@gmail.com>
2023-07-19 14:11:28 +0300

    Add button

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
-console.log("Hello World");
 
-const createGrid = () => {
+/* Function to create the grid */
+const createGrid = (num) => {
 
+  num = 16;
+ 
   /* Assigning the container from The HTML file */
   var container = document.getElementById("container");
 
   /* creating 16 rows */
-  for (let i = 0; i < 16; i++) {
+  for (let i = 0; i < num; i++) {
     var row = document.createElement("div");
     row.className = "row";
 
     /* Loop to create 16 squares inside each row */
-    for (let j = 0; j < 16; j++) {
+    for (let j = 0; j < num; j++) {
       let square = document.createElement("div");
       square.className = "square";
       square.addEventListener("mouseover", function () {
@@ -25,4 +27,24 @@ const createGrid = () => {
   
 }
 
-createGrid();
+const createButton = () => {
+  var button = document.createElement("button");
+    button.id = "btn";
+  button.textContent = "Enter the number of pixels you want per row & column";
+  return button;
+}
+
+
+const initializePage = () => {
+  var heading = document.createElement("h1");
+  heading.textContent = "Etch A Sketch";
+  document.body.appendChild(heading);
+
+  var button = createButton();
+  document.body.appendChild(button);
+
+  createGrid();
+}
+
+/* Calling the function to initialize the page */
+initializePage();
